9d3f0839a085dd84fcdc038fce1fc98279f6fe11
alimbdit <alimbdit@gmail.com>
2024-05-07 08:24:26 +0600

    access modifier added

---
+++ module3/3.5.ts
@@ -0,0 +1,82 @@
+{
+    //& Access modifiers
+
+    class BankAccount {
+        readonly id: number;
+        public name: string;
+        private _email: string;
+        protected _balance: number;
+
+        constructor(id: number, name: string, email: string, balance: number) {
+            this.id = id;
+            this.name = name;
+            this._email = email;
+            this._balance = balance;
+        }
+
+        public addDeposit(amount: number) {
+            this._balance = this._balance + amount;
+        }
+        public changeEmail(mail: string) {
+            this._email = mail;
+        }
+
+        public getBalance() {
+            return this._balance;
+        }
+
+    }
+
+    class StudentAccount extends BankAccount {
+        // test(){
+        //     this. 
+        // }  
+        //& here after this. email will not get because it is private not and _balance can get as it is protected. so protected can inherit to child class, on other hand private(only can use in the own class) can not be inherited.
+
+        // constructor(id: number, name: string, email: string, balance: number) {
+        //     super(id, name, email, balance)
+        // }
+    }
+
+
+    const goribManushAccount = new BankAccount(111, 'poor man', 'poor@LockManager.com', 20);
+
+    goribManushAccount.changeEmail('gorib@manush@mail.com')
+
+    // goribManushAccount.balance=
+
+    const balance1 = goribManushAccount.getBalance();
+    goribManushAccount.addDeposit(20);
+    const balance2 = goribManushAccount.getBalance();
+
+    console.log(balance1, balance2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // 
+}
